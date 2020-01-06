cedced1db8482648daa5b878e6a8b1eec3fdbabf
kpose <ganihujude@gmail.com>
2020-01-06 12:00:20 +0100

    load posts from API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 //components
 import Navbar from './components/Navbar'
 
@@ -10,9 +11,30 @@ import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#339ba5',
+      main: '#00838f',
+      dark: '#005b64',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#f6734b',
+      main: '#f4511e',
+      dark: '#aa3815',
+      contrastText: '#000',
+    }
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
+
 function App() {
   return (
-    <div className="App">
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
       <Router>
       <Navbar />
         <div className="container">
@@ -25,6 +47,7 @@ function App() {
 
       </Router>
     </div>
+    </MuiThemeProvider>
   );
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+
 
 export class Home extends Component {
+    state = {
+        posts: null
+    }
+    componentDidMount() {
+        axios.get('https://us-central1-heyoo-9a975.cloudfunctions.net/api/posts')
+            .then(res => {
+                console.log(res.data)
+                this.setState({
+                    posts: res.data 
+                });
+            })
+            .catch(err => console.log(err));
+    }
     render() {
+        let recentPosts = this.state.posts ? (
+            this.state.posts.map(post => <p>{post.body}</p>)
+        ) : <p>Loading...</p>
         return (
-            <div>
-                <h1>Home Page</h1>
-            </div>
+            <Grid container spacing={10}>
+                <Grid item sm={8} xs={12}>
+                    {recentPosts}
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                    <p>Profile</p>
+                </Grid>
+            </Grid>
         )
     }
 }
